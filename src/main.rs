ca6a5cfc7ba476c0f69c261b0cdd7445c08b1ff3
jfecher <jfecher11@gmail.com>
2020-03-26 23:06:57 -0400

    Iterate through windows and add them to the list

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,54 @@
 #[cfg(windows)]
 extern crate winapi;
 
-use std::io::Error;
+// use std::io::Error;
 
 // SetWindowPos(HWND window, HWND insertAfter, int x, int y, int sizeX, int sizeY, uint flags) -> bool success
 use winapi::um::winuser::SetWindowPos;
+
+use winapi::shared::windef::HWND;
 
 // EnumWindows(WNDENUMPROC, LPARAM) -> bool
 // type WNDENUMPROC = Option ( HWND -> LPARAM -> bool )
 // type LPARAM = LONG_PTR = isize
 use winapi::um::winuser::EnumWindows;
 
-#[cfg(windows)]
-fn print_message(msg: &str) -> Result<i32, Error> {
-    use std::ffi::OsStr;
-    use std::iter::once;
-    use std::os::windows::ffi::OsStrExt;
-    use std::ptr::null_mut;
-    use winapi::um::winuser::{MB_OK, MessageBoxW};
-    let wide: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
-    let ret = unsafe {
-        MessageBoxW(null_mut(), wide.as_ptr(), wide.as_ptr(), MB_OK)
-    };
-    if ret == 0 { Err(Error::last_os_error()) }
-    else { Ok(ret) }
+use winapi::um::winuser::*;
+
+unsafe extern "system"
+fn add_window(window: HWND, args: isize) -> i32 {
+    let visible = IsWindowVisible(window) != 0;
+    let title = get_window_name(window);
+
+    if visible && !title.is_empty() {
+        let windows: &mut Vec<HWND> = std::mem::transmute(args);
+        windows.push(window);
+    }
+    1
 }
-#[cfg(not(windows))]
-fn print_message(msg: &str) -> Result<(), Error> {
-    println!("{}", msg);
-    Ok(())
+
+fn get_window_name(window: HWND) -> String {
+    let len = unsafe { GetWindowTextLengthA(window) + 1 };
+    let mut buf = vec![0; len as usize];
+    unsafe { GetWindowTextA(window, buf.as_mut_ptr(), len) };
+    let mut buf: Vec<u8> = buf.iter().map(|&i| i as u8).collect();
+    buf.pop();
+    let s = String::from_utf8(buf).unwrap_or("".to_owned());
+    String::from(s.trim())
 }
+
 fn main() {
-    print_message("Hello, world!").unwrap();
+    let mut windows: Vec<HWND> = Vec::new();
+    unsafe {
+        EnumWindows(Some(add_window), std::mem::transmute(&windows));
+
+        let null = std::mem::transmute(0isize);
+        let mut x = 0;
+        for window in windows.iter_mut() {
+            SetWindowPos(*window, null, x, 200, 500, 500, 0);
+            println!("Tiling '{}'", get_window_name(*window));
+            x += 100;
+        }
+        println!("\nTiled {} windows", windows.len());
+    }
 }
